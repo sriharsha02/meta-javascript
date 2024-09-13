c1d872789fa1287558880de30b7a69b246fad3c5
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2024-09-13 11:53:36 +0530

    Initial commit

---
+++ variables.js
@@ -0,0 +1,30 @@
+console.log("Hello");
+
+var myName = "Sri Harsha";
+
+console.log("Hello", myName);
+
+myName = "Sri";
+
+console.log("Hello", myName);
+
+//Exercise:
+
+var petDog = "Rex";
+var petCat = "Pepper";
+
+console.log(petDog);
+console.log(petCat);
+
+console.log("My pet dogs's name is:", petDog);
+console.log("My pet cat's name is:", petCat);
+
+var catSound = "purr";
+var dogSound = "woof";
+
+console.log(petDog, "says", dogSound);
+console.log(petCat, "says", catSound);
+
+catSound = "meow";
+
+console.log(petCat, "now says", catSound);
