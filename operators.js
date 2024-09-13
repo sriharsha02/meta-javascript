1d9629e7ad38578d4b8ba13585372e80e50cb65a
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2024-09-13 12:18:08 +0530

    operators

---
+++ operators.js
@@ -0,0 +1,40 @@
+/*Arithmetic operators
+
+ addition  2 + 3
+
+ subtraction  3 - 2
+
+ division  35 / 5
+
+ multipllication  7 * 4
+
+*/
+
+/*Comparison operators
+
+ greaterThan  3 > 2;
+ lessThan  2 < 3;
+ notEqualTo  5 != 6;
+ equalTo  5 == 5;
+
+*/
+
+/* 
+Logical Operators
+
+&&
+
+||
+
+!
+
+*/
+
+console.log(2 + 2);
+console.log(1 + 2 + 3 + 4 + 5);
+console.log(20 - 18);
+console.log(2 * 3);
+console.log(8 / 1);
+console.log(3 > 2);
+console.log(2 > 3);
+console.log(10 == 10);
