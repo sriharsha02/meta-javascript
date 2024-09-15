1630282c95eb55b0c6c61d1c802194dd2b456f74
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2024-09-15 14:12:36 +0530

    math functions in JS
    random () and ceil()

---
+++ math.js
@@ -0,0 +1,25 @@
+//Math.random
+
+var decimal = Math.random();
+console.log(decimal);
+
+console.log(10 * decimal);
+
+//Match.ceil
+
+// var rounded = Math.ceil(0.0001);
+// var rounded = Math.ceil(0.5);
+// var rounded = Math.ceil(0.99);
+// var rounded = Math.ceil(1.01);
+// var rounded = Math.ceil(1.5);
+var rounded = Math.ceil(2.99);
+
+console.log(rounded);
+
+//
+
+var randomNumber = Math.random() * 10;
+
+var roundedNumber = Math.ceil(randomNumber);
+
+console.log(roundedNumber);
