818fc9d249ad26a6de5731ecb3eead8b5b089fa3
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2024-09-15 15:46:01 +0530

    undefined, null, empty

---
+++ undefined-null-empty.js
@@ -0,0 +1,21 @@
+//null
+
+var newString = "abc";
+
+console.log(newString.match("a"));
+console.log(newString.match("d")); //gives null
+
+//undefined
+
+var noise;
+
+console.log(noise); //undefined
+
+noise = "high";
+
+console.log(noise);
+
+//empty
+
+var string1 = "";
+console.log(string1);
