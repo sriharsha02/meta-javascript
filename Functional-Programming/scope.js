66a6f4bcd825355cb3297103413d91499d063c5b
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2024-09-17 17:27:11 +0530

    scope example

---
+++ Functional-Programming/scope.js
@@ -0,0 +1,6 @@
+//Scope is about accesability of code
+
+var globalVar = "I am in a global scope."; // in global scope
+function test() {
+  var localVar = "I am function scoped."; //local sccope
+}
