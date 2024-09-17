433f8f5b60f4eb021b62065ee3bf40828e1d77c7
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2024-09-17 17:12:52 +0530

    recursion example

--- Functional-Programming/convertCurrency.js
+++ Functional-Programming/convertCurrency.js
@@ -9,3 +9,40 @@ function convertCurrency(amount, rate) {
 currencyTwo = convertCurrency(currencyOne, exchangeRate);
 
 console.log(currencyTwo);
+
+//return
+
+function doubleIt(num) {
+  return num * 2;
+}
+
+function turnIntoObject(val) {
+  return {
+    property: val,
+  };
+}
+
+var answer = turnIntoObject(doubleIt(100));
+
+console.log(answer);
+
+//recursion
+
+function exampleOne() {
+  console.log(1);
+  console.log(2);
+  console.log(3);
+}
+
+exampleOne();
+
+//
+let counter = 3;
+function exampleTwo() {
+  console.log(counter);
+  counter = counter - 1;
+  if (counter === 0) return;
+  exampleTwo();
+}
+
+exampleTwo();
