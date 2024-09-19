f85338f21564976d0121104aee6402b46518e545
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2024-09-19 12:25:12 +0530

    template literals
    multiline strings
    interpolate variables

---
+++ Advanced-js-features.js/TemplateLiterals.js
@@ -0,0 +1,25 @@
+//variable interpolation
+
+let greet = "Hello";
+let place = "world";
+console.log(`${greet} ${place}`);
+
+//evaluate an expression
+
+console.log(`${1 + 1 + 1 + 1} stars are present`);
+
+//ES5 strings
+
+let noMultiLine = "No multi line strings in ES5";
+console.log("Did you know?", noMultiLine);
+
+//ES6 multi line strings
+
+let multiLine = ` 
+    Using 
+    ES6 
+    multi lines 
+    are 
+    possible.`;
+
+console.log(multiLine);
