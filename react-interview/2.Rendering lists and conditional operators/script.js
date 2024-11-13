329321990b0d5e5159ffabfc0c6a303458c1d9cf
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2024-11-13 14:24:13 +0530

    map filter reduce in react

---
+++ react-interview/2.Rendering lists and conditional operators/script.js
@@ -0,0 +1,103 @@
+const products = [
+  { id: 1, name: "Product A", price: 20, category: "Electronics" },
+  { id: 2, name: "Product B", price: 30, category: "Clothing" },
+  { id: 3, name: "Product C", price: 15, category: "Electronics" },
+  { id: 4, name: "Product D", price: 25, category: "Clothing" },
+  { id: 5, name: "Product E", price: 50, category: "Electronics" },
+  { id: 6, name: "Product F", price: 40, category: "Electronics" },
+];
+
+const names = ["Alice", "Bob", "Alice", "Charlie", "Bob"];
+function App() {
+  return (
+    <div>
+      <h2>Rendering Lists and conditional operators</h2>
+      <h5>
+        Question 1: How does the map function work in React for rendering lists?
+        Can you provide an example?
+      </h5>
+      {/*
+        - Commonly used to iterate through an array and render components dynamically.
+        - It allows you to create a new array of React elements based on the original array. 
+        */}
+      <ul>
+        {products.map((product) => {
+          return (
+            <div>
+              <li key={product.id}>
+                <strong>{product.name}</strong> - ${product.price} - Category:{" "}
+                {product.category}
+              </li>
+            </div>
+          );
+        })}
+      </ul>
+      <h5>Question 2:How can you filter products with a specific category?</h5>
+      <ul>
+        {products
+          .filter((product) => product.category === "Electronics")
+          .map((product) => {
+            return (
+              <div>
+                <li>
+                  <strong>{product.name}</strong> - ${product.price} - Category:{" "}
+                  {product.category}
+                </li>
+              </div>
+            );
+          })}
+      </ul>
+      <h5>
+        Question 3:How can you render a summary of total prices for products?
+      </h5>
+      <div>
+        <p>
+          Total Price Summary: $
+          {products.reduce((acc, product) => {
+            return acc + product.price;
+          }, 0)}
+        </p>
+      </div>
+      <h5>
+        Question 4: Add discountedPrice key with 10% discount to all the
+        products with price more than $20 and render it.
+      </h5>
+      {products
+        .filter((product) => {
+          return product.price > 20;
+        })
+        .map((product) => {
+          return {
+            ...product,
+            discountedPrice: product.price * 0.1,
+          };
+        })
+        .map((product) => {
+          return (
+            <div>
+              <li key={product.id}>
+                <strong>{product.name}</strong> - ${product.discountedPrice} -
+                Discounted from:
+                {product.price}
+              </li>
+            </div>
+          );
+        })}
+
+      <h5>
+        Question 5: How can you filter and render unique elements from an array
+        using filter in React?
+      </h5>
+      {names
+        .filter((name, index) => {
+          return names.indexOf(name) === index;
+        })
+        .map((name) => {
+          return <li>{name}</li>;
+        })}
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
