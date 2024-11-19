bc8a8bc56a59518d02dff06d9624b139359119cf
Akshintala Sri Harsha <akshintalasriharsha@yahoo.com>
2024-11-19 23:25:42 +0530

    useEffect

--- react-interview/4.hooks/src/App.jsx
+++ react-interview/4.hooks/src/App.jsx
@@ -1,11 +1,13 @@
 
 import "./App.css" 
 import UseStateHook from "./components/use-state-hook.jsx"
+import UseEffectHook from "./components/use-effect-hook.jsx"
 function App() {
   return (
     <div>
       <h2>Hooks Interview Questions</h2>
       <UseStateHook />
+      <UseEffectHook />
     </div>
   ) 
 }
---
+++ react-interview/4.hooks/src/components/use-effect-hook.jsx
@@ -0,0 +1,13 @@
+
+
+const UseEffectHook = () => {
+  return (
+    <div> 
+        <h3>useEffect Hook</h3>
+        <h5>Question 1: What is useEffect hook in React?</h5>
+        <p>useEffect is a hook used in functional components to perform side effects after rendering such as data fetching, subscriptions, manual DOM manipulations.</p>
+    </div>
+  )
+}
+
+export default UseEffectHook;
